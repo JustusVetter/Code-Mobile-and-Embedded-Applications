07eae395535dfdcf9c96176b83ed8f4a0626b452
Justus <vetter.justus.a23@dh-karlsruhe.de>
2025-12-11 23:15:02 +0100

    feat: button change view and new view

--- mandatory_exercise_02/app/src/main/java/edu/urv/lifeify/SectionsActivity.java
+++ mandatory_exercise_02/app/src/main/java/edu/urv/lifeify/SectionsActivity.java
@@ -1,8 +1,6 @@
 package edu.urv.lifeify;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SectionsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sections);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnLogin= findViewById(R.id.btnLogin);
-
-        btnLogin.setOnClickListener( click -> {
-                        Intent intent = new Intent(MainActivity.this, SectionsActivity.class);
-                        startActivity(intent);
-            }
-        );
     }
 }
