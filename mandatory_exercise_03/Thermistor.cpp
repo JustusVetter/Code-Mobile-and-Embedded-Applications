313d52e35d08c535c73123fd08af79e579188f68
Justus <vetter.justus.a23@dh-karlsruhe.de>
2026-01-26 14:02:08 +0100

    feat: embed now works
    start now implementing bluetooth part in android and libraries in mbed

--- mandatory_exercise_03/Thermistor.cpp
+++ mandatory_exercise_03/Thermistor.cpp
@@ -10,19 +10,18 @@ Thermistor::Thermistor(PinName pin) : _pin(pin){
 float Thermistor::read(){
     ThisThread::sleep_for(_defaultDelay);
     float f =  _pin.read();
-    //printf("Read from pin: %f\n",f);
-    float rTherm = _rb * ((1.0 / f) - 1);//3.3
-    //float rTherm = 100000.0 * ((1.0 / f) - 1);
-    float tKelvin = (1.0 / ((log(rTherm/_rz) / _beta) + (1.0/_tz)));//b:4250;tz:298.15;_rz:100000
-    //printf("%f\n",rTherm);
-    //float tKelvin = (1.0 / ((log(rTherm/100000.0) / 4250.0 )+ (1.0/298.15)));
+    
+    float rTherm = _rb * ((1.0 / f) - 1);
+    
+    float tKelvin = (1.0 / ((log(rTherm/_rz) / _beta) + (1.0/_tz)));
+    
     float result = 1;
     if (_enableKelvin) {
         result = tKelvin;
     }else {
         result = tKelvin - KONVERT_KELVIN;
+        result = result/2;
     }
-    printf("%f\n",tKelvin);
     return result;
 }
 
--- mandatory_exercise_03/main.cpp
+++ mandatory_exercise_03/main.cpp
@@ -2,18 +2,25 @@
 #include "BufferedSerial.h"
 #include "ThisThread.h"
 #include <ByteBuffer.h>
+#include "Thermistor.h"
+
 BufferedSerial bs(D1,D0,9600);
+Thermistor th(A0);
 // main() runs in its own thread in the OS
 int main()
 {
-bs.set_format(
-/* bits */ 8,
-/* parity */ BufferedSerial::None,
-/* stop bit */ 1
-);
-while (true) {
-double buf[2] = {0};
-bs.write(buf, sizeof(buf));
-ThisThread::sleep_for(1000ms);
-}
+    bs.set_format(
+    /* bits */ 8,
+    /* parity */ BufferedSerial::None,
+    /* stop bit */ 1
+    );
+    double buf[2] = {'0'};
+    while (true) {
+        double temp = th.read();
+        buf[0]=temp;
+        //temp = std::round(temp * 100.0) / 100.0;
+        //int len = snprintf(buf, sizeof(buf), "tmp=%.2f \t", temp);
+        bs.write(buf, sizeof(buf));
+        ThisThread::sleep_for(1000ms);
+    }
 }
